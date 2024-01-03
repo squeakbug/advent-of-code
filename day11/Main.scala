775e0b3b4efeebf2f6c2338da18012c01204c186
squeakbug <squeakbug73@outlook.com>
2024-01-04 01:46:18 +0300

    [Day11] part2

--- day11/Main.scala
+++ day11/Main.scala
@@ -5,17 +5,17 @@ object Main {
     line.count((sym) => sym == '#') == 0
   }
 
-  def upperFree(lines: Array[String], yPos: Int): Int = {
+  def upperFree(lines: Array[String], yPos: Int): Long = {
     lines.take(yPos).count((line) => isFreeRow(line))
   }
 
-  def leftFree(lines: Array[String], xPos: Int): Int = {
+  def leftFree(lines: Array[String], xPos: Int): Long = {
     (0 until xPos)
       .map((pos) => lines.map((line) => line(pos)))
       .count((line) => isFreeRow(String(line)))
   }
 
-  def solve(lines: Array[String]): Int = {
+  def solve(lines: Array[String], factor: Long): Long = {
     var yRange = 0 until lines.length;
     var xRange = 0 until lines(0).length;
 
@@ -23,24 +23,24 @@ object Main {
       .flatMap((y) => xRange.map((x) => (y, x)))
       .filter((pos) => lines(pos._1)(pos._2) == '#')
       .map((pos) =>
-        (pos._1 + upperFree(lines, pos._1),
-         pos._2 + leftFree(lines, pos._2)))
+        (pos._1 + (factor - 1) * upperFree(lines, pos._1),
+         pos._2 + (factor - 1) * leftFree(lines, pos._2)))
       .tails
       .filter(_.nonEmpty)
       .flatMap(xs => xs.tail.map((xs.head, _)))
-      .foldLeft(0)((acc, pair) =>
+      .foldLeft(0.toLong)((acc, pair) =>
         acc
         + Math.abs(pair._2._1 - pair._1._1) 
         + Math.abs(pair._2._2 - pair._1._2)
       )
   }
 
-  def part1(lines: Array[String]): Int = {
-    return solve(lines);
+  def part1(lines: Array[String]): Long = {
+    return solve(lines, 2);
   }
 
-  def part2(lines: Array[String]): Int = {
-    return solve(lines)
+  def part2(lines: Array[String]): Long = {
+    return solve(lines, 1_000_000)
   }
 
   def main(args: Array[String]): Unit = {
